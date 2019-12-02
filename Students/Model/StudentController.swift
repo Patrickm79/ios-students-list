ac4b542b41bb71646b87c35cc288a04d8d3280ed
Patrick Millet <lohs79seahawk@gmail.com>
2019-12-02 13:18:38 -0600

    added sort and filter functionality to the .json list

--- Students/Model/StudentController.swift
+++ Students/Model/StudentController.swift
@@ -8,10 +8,69 @@
 
 import Foundation
 
+enum SortOptions: Int {
+    case firstName
+    case lastName
+}
+
+enum TrackType: Int {
+    case none
+    case iOS
+    case Web
+    case UX
+}
+
 class StudentController {
+    
+    private var students: [Student] = []
     
     private var persistentFileURL: URL? {
         guard let filePath = Bundle.main.path(forResource: "students", ofType: "json") else { return nil }
         return URL(fileURLWithPath: filePath)
+    }
+    
+    
+    func loadFromPersistentStore(completion: @escaping ([Student]?, Error?) -> Void) {
+        let bgQueue = DispatchQueue(label: "stuedntQueue", attributes: .concurrent)
+        
+        bgQueue.async {
+            let fm = FileManager.default
+            guard  let url = self.persistentFileURL,
+                fm.fileExists(atPath: url.path) else { return }
+            
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let students = try decoder.decode([Student].self, from: data)
+                self.students = students
+                completion(students, nil)
+            } catch {
+                print("Error loading student data: \(error)")
+                completion(nil, error)
+            }
+        }
+    }
+    
+    func filter(with trackType: TrackType, sortedBy sorter: SortOptions, completion: @escaping ([Student]) -> Void) {
+        var updatedStudents: [Student]
+        
+        switch trackType {
+        case .iOS:
+            updatedStudents = students.filter { $0.course == "iOS" }
+        case .Web:
+            updatedStudents = students.filter { $0.course == "Web" }
+        case .UX:
+            updatedStudents = students.filter { $0.course == "UX" }
+        default:
+            updatedStudents = students
+        }
+        
+        if sorter == .firstName {
+            updatedStudents = updatedStudents.sorted { $0.firstName < $1.firstName }
+        } else {
+            updatedStudents = updatedStudents.sorted { $0.lastName < $1.lastName }
+        }
+        
+        completion(updatedStudents)
     }
 }
